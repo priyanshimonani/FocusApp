931df38b0fb0b5404d3b0c8a6e233a3f5cdd053c
paninishah <paninishah29@gmail.com>
2025-10-18 20:57:27 +0530

    add todo page tried to have different visual levels for priority

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Landing from './pages/Landing/Landing';
 import Pomodoro from './pages/Pomodoro/Pomodoro';
+import TodoList from './pages/TodoList/TodoList';
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/pomodoro" element={<Pomodoro />} />
+            <Route path="/todo" element={<TodoList/>}/>
           </Routes>
     </BrowserRouter>
   );
